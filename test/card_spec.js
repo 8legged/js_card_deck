c9310f827f0209f51137b9005721cf62d69bb79e
8legged <jason.niemi@live.com>
2014-02-17 15:24:06 -0800

    updated tests

---
+++ test/card_spec.js
@@ -0,0 +1,29 @@
+var expect = require('chai').expect;
+
+var Card = require('../lib/card').Card;
+describe('Card object tests', function() {
+'use strict';
+  var card;
+  beforeEach(function() {
+    card = new Card('10', 'Spades');
+  });
+
+  describe('constructor', function() {
+    it('card should be truthy(exists)', function() {
+      expect(card).to.be.ok;
+    });
+
+    it('card should have rank property', function() {
+      expect(card.getRank()).to.equal('10');
+    });
+
+    it('card should have correct suit', function() {
+      expect(card.getSuit()).to.equal('Spades');
+    });
+
+    it('can\'t change the card suit', function() {
+      card.suit = 'Hearts';
+      expect(card.getSuit()).to.equal('Spades');
+    });
+  });
+});
